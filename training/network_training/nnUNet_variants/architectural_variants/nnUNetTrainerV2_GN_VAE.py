6b4301ca1337a9d02c80954cdb6da853014a8395
Ryle Zhou <rylezhou@gmail.com>
2021-06-26 01:27:27 -0700

    add

--- training/network_training/nnUNet_variants/architectural_variants/nnUNetTrainerV2_GN_VAE.py
+++ training/network_training/nnUNet_variants/architectural_variants/nnUNetTrainerV2_GN_VAE.py
@@ -77,12 +77,14 @@ class nnUNetTrainerV2_GN_VAE(nnUNetTrainerV2):
         :return:
         """
         data_dict = next(data_generator)
-        data = data_dict['data'] ## input
+        # data = data_dict['data'] ## input
+        data = np.random.rand(2,1,512, 780, 172)
         target = data_dict['target'] ##seg target
         # print([t.shape for t in target])
       
 
         data = maybe_to_torch(data)
+        # DEBUG:([2, 1, 80, 224, 128])
         print("--------------------------input_shape", data.shape)
     
         target = maybe_to_torch(target)

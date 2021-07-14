bfbad3f1e0a81bb3b310463069c01fd88520180a
Ryle Zhou <rylezhou@gmail.com>
2021-07-14 08:05:56 -0700

    cascade

--- training/network_training/nnUNetTrainerV2_CascadeFullRes.py
+++ training/network_training/nnUNetTrainerV2_CascadeFullRes.py
@@ -38,7 +38,7 @@ matplotlib.use("agg")
 
 class nnUNetTrainerV2CascadeFullRes(nnUNetTrainerV2):
     def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, batch_dice=True, stage=None,
-                 unpack_data=True, deterministic=True, previous_trainer="nnUNetTrainerV2", fp16=False):
+                 unpack_data=True, deterministic=True, previous_trainer="nnUNetTrainerV2_GN_VAE", fp16=False):
         super().__init__(plans_file, fold, output_folder, dataset_directory,
                          batch_dice, stage, unpack_data, deterministic, fp16)
         self.init_args = (plans_file, fold, output_folder, dataset_directory, batch_dice, stage, unpack_data,
